3103c5998034a1c0fc490ab2df93c2bafebe772f
Lukaka4331 <105021044@live.asia.edu.tw>
2016-09-26 15:46:05 +0800

    ex~~

---
+++ src/ex/ex02_105021044.java
@@ -0,0 +1,48 @@
+package ex;
+import java.util.Scanner;
+/*
+ * Topic: 有一圓形，直徑為200，且中心座標為(0,0)。
+ * 	請寫一支程式可以輸入「點」的座標，並判斷「點」是否在圓形的範圍內。
+ * 	如果「點」的位置剛好在邊界的話也算是在圓形範圍內(例：x=100，y=0)。
+ * Date: 2016/09/26
+ * Author: 105021044 鐘岑哲
+ */
+
+public class ex02_105021044 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn= new Scanner(System.in);
+		System.out.println("a=");
+		System.out.println("b=");
+		int x= 0;
+		int y= 0;
+		int a= scn.nextInt();
+		int b= scn.nextInt();
+int d= (int)Math.sqrt(c); 
+		if(d<=100){
+		System.out.println("在裡面");
+		
+		
+			}else{		System.out.println("在外面");
+
+				
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
