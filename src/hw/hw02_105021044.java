53cb6e90b693067aafb75d9c18dc617078ca0e2f
Lukaka4331 <105021044@live.asia.edu.tw>
2016-10-02 11:57:47 +0800

    hw02 new

--- src/hw/hw02_105021044.java
+++ src/hw/hw02_105021044.java
@@ -1,6 +1,8 @@
 package hw;
 import java.util.Scanner;
-public class hw02_1050210XX {
+//已知男生標準體重＝(身高－80 )*0.7；女生標準體重＝(身高－70)*0.6；試寫一個程式可以計算男生女生的標準體重。(輸入兩個數值，依序代表為身高及性別（1代表男性；2代表女性）)
+//105021044 鐘岑哲
+public class hw02_105021044 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
